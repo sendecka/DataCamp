1a41d281a4bfb2ddb5e9f21cc370b3573f2d07d2
sendecka <103881908+sendecka@users.noreply.github.com>
2023-12-14 20:38:20 +0000

    14-12-2023

--- DataCamp/01 Introduction to Python/01 Python Basics.py
+++ DataCamp/01 Introduction to Python/01 Python Basics.py
@@ -473,3 +473,44 @@ print(type(np_baseball))
 # Print np_height_in.
 # Multiply np_height_in with 0.0254 to convert all height measurements from inches to meters. Store the new values in a new array, np_height_m.
 # Print out np_height_m and check if the output makes sense.
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
+# Convert np_height_in to m: np_height_m
+np_height_m = np_height_in * 0.0254
+
+# Print np_height_m
+print(np_height_m)
+
+
+### Baseball player's BMI
+# Instructions:
+# Create a numpy array from the weight_lb list with the correct units. Multiply by 0.453592 to go from pounds to kilograms. Store the resulting numpy array as np_weight_kg.
+# Use np_height_m and np_weight_kg to calculate the BMI of each player. Use the following equation:
+ 
+# Save the resulting numpy array as bmi.
+# Print out bmi.
+
+# Import numpy
+import numpy as np
+
+# Create array from height_in with metric units: np_height_m
+np_height_m = np.array(height_in) * 0.0254
+
+# Create array from weight_lb with metric units: np_weight_kg
+np_weight_kg = np.array(weight_lb) * 0.453592
+
+# Calculate the BMI: bmi
+bmi = np_weight_kg / np_height_m**2
+
+# Print out bmi
+print(bmi)
+
+
